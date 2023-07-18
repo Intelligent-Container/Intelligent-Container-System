248f3c857f2087a5adf94509b776d2217500a626
3liujin <114408081+3liujin@users.noreply.github.com>
2023-07-18 11:11:12 +0800

    更新数据解析、数据库代码
    更新数据解析、数据库代码

--- 2K1000/database/data_parse.py
+++ 2K1000/database/data_parse.py
@@ -5,29 +5,45 @@ def data_parse(json_str):
     obj = json.loads(json_str)
     A = obj['A']  # 报头
     B = obj['B']  # MAC地址
-    C = obj['C']  # 电量
-    D = obj['D'][0] + "-" + obj['D'][1] + "-" + obj['D'][2]  # 年月日
-    E = obj['E'][0] + ":" + obj['E'][1] + ":" + obj['E'][2]  # 时分秒
-    temp_str = obj['F']['F1']  # 温度
-    F1 = temp_str[:temp_str.index('.') + 2]
+    num = B.split(':')
+    num_str = num[3] + num[4]
+    id = int(num_str, 16)
+    # C = obj['C']  # 电量
 
-    humi_str = obj['F']['F2']  # 湿度
-    F2 = humi_str[:humi_str.index('.')]
+    D0 = obj['D'][0]
+    D1 = obj['D'][1]
+    D2 = obj['D'][2]
 
-    smoke_str = obj['F']['F3']  # 烟雾报警
-    smoke = int(smoke_str)
-    if 5 <= smoke <= 15:
-        F3 = 1
-    else:
-        F3 = 0
+    E0 = obj['E'][0]
+    E1 = obj['E'][1]
+    E2 = obj['E'][2]
+    D = D0 + "-" + D1 + "-" + D2  # 年月日
+    E = E0 + ":" + E1 + ":" + E2  # 时分秒
+    F1 = obj['F']['F1']  # 温度
+    # F1 = temp_str[:temp_str.index('.') + 2]
 
-    press_str = obj['F']['F4']  # 气压
-    F4 = press_str[:press_str.index('.')]
+    F2 = obj['F']['F2']  # 湿度
 
-    F5 = obj['F']['F5']  # 经纬度
-    G = obj['G']  # RFID信息 暂定16位字符串
-    H = obj['H']  # 校验位
+    F3 = obj['F']['F3']  # 烟雾报警
+
+    F4 = obj['F']['F4']  # 气压
+    # F4 = press_str[:press_str.index('.') + 2]
+    F5 = obj['F']['F5']  # 光强
+
+    # G = obj['G']  # RFID信息 暂定16位字符串
+    # H = obj['H']  # 校验位
     I = obj['I']  # 停止位
-    data = [A, B, C, D, E, F1, F2, F3, F4, F5, G, H, I]
-    # print(data)
+
+    # checkstr = A + ":" + B + ":" + C + ":" + D0 + ":" + D1 + ":" + D2 + ":" + E0 + ":" + E1 + ":" + E2 + ":" + F1 + ":" + F2 + ":" + F3 + ":" + F4 + ":" + F5 + ":" + F6 + ":" + G + ":" + I
+    # checksum = check.calculate_checksum(checkstr)
+    # if int(H) == checksum:
+    #     print("数据校验成功")
+    #     data = [A, B, C, D, E, F1, F2, F3, F4, F5, F6, G, H, I]
+        # print(data)
+    #     return data
+    # else:
+    #     print("数据校验失败")
+    #     return None
+    F6 = "112.23&123.34"  # 经纬度
+    data = [A, id, B, D, E, F1, F2, F3, F4, F5, F6, I]
     return data
--- 2K1000/database/db.py
+++ 2K1000/database/db.py
@@ -15,7 +15,7 @@ def get_table_name():
     conn = sqlite3.connect('test.db')
     cursor = conn.cursor()
     # 查询最新生成的数据库表名
-    sql = "select name from sqlite_master where type = 'table' order by name desc limit 1;"
+    sql = "select name from sqlite_master where type = 'table' and name != 'device' order by name desc limit 1;"
     try:
         cursor.execute(sql)
         ret1 = cursor.fetchone()
@@ -33,28 +33,30 @@ def create_table(table_name):
     conn = sqlite3.connect('test.db')
     cursor = conn.cursor()
     sql_drop = 'drop table if exists ' + table_name
-    #cursor.execute(sql_drop)
-    sql_create = 'create table if not exists ' + table_name + '(addr varchar(50),' \
-                                                              'power int,' \
+    sql_drop_device = 'drop table if exists device'
+    # cursor.execute(sql_drop)
+    sql_create = 'create table if not exists ' + table_name + '(id int,addr varchar(50),' \
                                                               'day date,' \
                                                               'time date,' \
-                                                              'temp double(4,1),' \
+                                                              'temp double,' \
                                                               'humi int,' \
                                                               'fire_alarm int,' \
                                                               'press int,' \
-                                                              'gps varchar(50),' \
-                                                              'rfid varchar(50),' \
-                                                              'verify varchar(20)) '
+                                                              'light double,' \
+                                                              'gps varchar(55))'
+    sql_create_device = 'create table if not exists device (device_id int, online bool);'
     # 异常处理
     try:
         # 创建一个表格
         cursor.execute(sql_create)
+        cursor.execute(sql_create_device)
         conn.commit()
         # 关闭数据库连接
         cursor.close()
         conn.close()
     except Exception as e:
         cursor.execute(sql_drop)
+        cursor.execute(sql_drop_device)
         conn.commit()
         # 关闭数据库连接
         cursor.close()
@@ -67,9 +69,27 @@ def insert(table_name, data):
     conn = sqlite3.connect('test.db')
     cursor = conn.cursor()
     # 插入一条记录
-    sql_insert = 'insert into ' + table_name + '(addr,power,day,time,temp,humi,fire_alarm,press,gps,rfid,verify) values (?,?,?,?,?,?,?,?,?,?,?)'
+    sql_insert = 'insert into ' + table_name + '(id,addr,day,time,temp,humi,fire_alarm,press,light,gps) values (?,?,?,?,?,?,?,?,?,?)'
     try:
         cursor.execute(sql_insert, data)
+        conn.commit()
+        # 关闭数据库连接
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        # 关闭数据库连接
+        cursor.close()
+        conn.close()
+        print(e)
+
+
+# 插入设备数据
+def insert_device(dev):
+    conn = sqlite3.connect('test.db')
+    cursor = conn.cursor()
+    sql_insert = 'insert into device(device_id,online) values(?,?)'
+    try:
+        cursor.execute(sql_insert, dev)
         conn.commit()
         # 关闭数据库连接
         cursor.close()
@@ -85,7 +105,7 @@ def insert(table_name, data):
 def select(table_name, column_name):
     conn = sqlite3.connect('test.db')
     cursor = conn.cursor()
-    sql = 'select '+ column_name+' from '+table_name +' where time = (select max(time) from '+ table_name + ' where day = (select max(day) from '+table_name+'))'
+    sql = 'select ' + column_name + ' from ' + table_name + ' where time = (select max(time) from ' + table_name + ' where day = (select max(day) from ' + table_name + '))'
     try:
         cursor.execute(sql)
         ret1 = cursor.fetchall()
@@ -108,8 +128,8 @@ def select_addr(table_name):
 
 
 # 查询电量
-def select_power(table_name):
-    return select(table_name, 'power')
+# def select_power(table_name):
+#     return select(table_name, 'power')
 
 
 # 查询年月日
@@ -142,6 +162,11 @@ def select_press(table_name):
     return select(table_name, 'press')
 
 
+# 查询光强
+def select_light(table_name):
+    return select(table_name, 'light')
+
+
 # 查询经纬度
 def select_gps(table_name):
     return select(table_name, 'gps')
@@ -151,26 +176,67 @@ def select_gps(table_name):
 def select_lon(table_name):
     gps = select_gps(table_name)
     lon_str = gps[:gps.index('&')]
-    lon = lon_str[:lon_str.index('.')]
-    return lon
+    return float(lon_str)
 
 
 # 查询纬度
 def select_lat(table_name):
     gps = select_gps(table_name)
     lat_str = gps[gps.index('&') + 1:]
-    lat = lat_str[:lat_str.index('.')]
-    return lat
+    # lat = lat_str[:lat_str.index('.')]
+    return float(lat_str)
 
 
-# 查询RFID信息
-def select_rfid(table_name):
-    return select(table_name, 'rfid')
+# 查询设备id是否存在
+def select_device(dev_id):
+    conn = sqlite3.connect('test.db')
+    cursor = conn.cursor()
+    sql = 'select * from device where device_id = ' + dev_id + ';'
+    try:
+        cursor.execute(sql)
+        ret = cursor.fetchall()
+        if ret:
+            return False
+        else:
+            return True
+    except Exception as e:
+        # 关闭数据库连接
+        cursor.close()
+        conn.close()
+        print(e)
 
 
-# 查询校验码
-def select_verify(table_name):
-    return select(table_name, 'verify')
+# 查询设备是否在线
+def select_device_online(dev_id):
+    conn = sqlite3.connect('test.db')
+    cursor = conn.cursor()
+    sql = 'select online from device where device_id = '+dev_id
+    try:
+        cursor.execute(sql)
+        ret = cursor.fetchone()
+        if ret:
+            if ret[0] == 1:
+                return "在线"
+            else:
+                return "离线"
+        else:
+            print("select_device1()查询失败")
+    except Exception as e:
+        # 关闭数据库连接
+        cursor.close()
+        conn.close()
+        print(e)
+
+
+
+# # 查询RFID信息
+# def select_rfid(table_name):
+#     return select(table_name, 'rfid')
+
+
+# # 查询校验码
+# def select_verify(table_name):
+#     return select(table_name, 'verify')
 
 
 # 修改数据
@@ -178,7 +244,7 @@ def update(table_name, column_name, column, line_name, line_id):
     conn = sqlite3.connect('test.db')
     cursor = conn.cursor()
     line = (line_name, line_id)
-    sql_update = 'update '+table_name+' set '+column_name + '=? where '+column+' =?'
+    sql_update = 'update ' + table_name + ' set ' + column_name + '=? where ' + column + ' =?'
     try:
         cursor.execute(sql_update, line)
         conn.commit()
@@ -186,7 +252,47 @@ def update(table_name, column_name, column, line_name, line_id):
         cursor.close()
         conn.close()
     except Exception as e:
-        #进行回滚
+        # 进行回滚
+        conn.rollback()
+        # 关闭数据库连接
+        cursor.close()
+        conn.close()
+        print(e)
+
+
+# 修改设备是否在线
+def update_device(dev_id, status):
+    conn = sqlite3.connect('test.db')
+    cursor = conn.cursor()
+    sql = 'update device set online = ' + status + ' where device_id = ' + dev_id
+    try:
+        cursor.execute(sql)
+        conn.commit()
+        # 关闭数据库连接
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        # 进行回滚
+        conn.rollback()
+        # 关闭数据库连接
+        cursor.close()
+        conn.close()
+        print(e)
+
+
+# 修改设备全部离线
+def update_device_offline():
+    conn = sqlite3.connect('test.db')
+    cursor = conn.cursor()
+    sql = 'update device set online = FALSE '
+    try:
+        cursor.execute(sql)
+        conn.commit()
+        # 关闭数据库连接
+        cursor.close()
+        conn.close()
+    except Exception as e:
+        # 进行回滚
         conn.rollback()
         # 关闭数据库连接
         cursor.close()
@@ -198,7 +304,7 @@ def update(table_name, column_name, column, line_name, line_id):
 def delete(table_name, column_name, column):
     conn = sqlite3.connect('test.db')
     cursor = conn.cursor()
-    sql_delete = 'delete from '+table_name+' where '+column_name+' =?'
+    sql_delete = 'delete from ' + table_name + ' where ' + column_name + ' =?'
     column = (column,)
     try:
         cursor.execute(sql_delete, column)
@@ -212,10 +318,5 @@ def delete(table_name, column_name, column):
         conn.close()
         print(e)
 
-
 # if __name__ == '__main__':
 #     print(get_table_name())
-
-
-
-
